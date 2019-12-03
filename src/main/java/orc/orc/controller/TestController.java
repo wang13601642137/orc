faa16d9bb4e6307b2b97829a22d6125c2f18eaf6
wenjin.wang <wenjin.wang@pintec.com>
2019-12-03 18:10:09 +0800

    first commit

---
+++ src/main/java/orc/orc/controller/TestController.java
@@ -0,0 +1,19 @@
+package orc.orc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by wenjin.wang on 2019/12/3.
+ */
+@Controller
+@RequestMapping(value = "/")
+public class TestController {
+
+    @RequestMapping(value="/test",produces="text/html;charset=UTF-8" )
+    @ResponseBody
+    private String test(){
+        return "ok";
+    }
+}
