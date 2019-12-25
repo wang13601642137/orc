54245e3549c65922228d81c8e24b16de8ebea720
wenjin.wang <wenjin.wang@pintec.com>
2019-12-25 18:59:08 +0800

    coding

--- create_table.sql
+++ create_table.sql
@@ -8,3 +8,20 @@ CREATE TABLE `admin` (
   `updated_by` int(11) NOT NULL COMMENT '更新人',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员';
+
+
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '会员编号，自增主键',
+  `user_name` varchar(50) NOT NULL COMMENT '用户名',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `real_name` varchar(50) DEFAULT NULL,
+  `nick_name` varchar(50) DEFAULT NULL COMMENT '昵称',
+  `mobile` varchar(11) DEFAULT NULL COMMENT '手机号',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `status` tinyint(1) NOT NULL COMMENT '状态：0 有效；1 无效',
+  `created_at` datetime NOT NULL COMMENT '创建时间',
+  `created_by` int(11) NOT NULL COMMENT '创建人',
+  `updated_at` datetime NOT NULL COMMENT '更新时间',
+  `updated_by` int(11) NOT NULL COMMENT '更新人',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='会员';
--- src/test/java/orc/orc/DaoTest.java
+++ src/test/java/orc/orc/DaoTest.java
@@ -1,11 +1,15 @@
 package orc.orc;
 
+import com.alibaba.fastjson.JSON;
 import orc.orc.domain.Admin;
 import orc.orc.repository.AdminRepository;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wenjin.wang on 2019/12/25.
@@ -17,12 +21,43 @@ public class DaoTest extends BaseTest{
     @Test
     public void testInsertAdmint() {
         Admin ad = new Admin();
-        ad.setPassword("admin");
-        ad.setUserName("admin");
+        ad.setPassword("admin2");
+        ad.setUserName("admin2");
         ad.setCreatedAt(new Date());
         ad.setCreatedBy(0);
         ad.setUpdatedAt(new Date());
         ad.setUpdatedBy(0);
         adminRepository.insert(ad);
+    }
+
+    @Test
+    public void testFind() {
+        Map<String, Object> map = new HashMap();
+        map.put("userName", "admin");
+        map.put("password", "admin");
+
+        List<Admin> adminList = adminRepository.find(map);
+        System.out.println(JSON.toJSONString(adminList));
+    }
+
+    @Test
+    public void testFindById() {
+        Admin admin = adminRepository.findById(1);
+        System.out.println(JSON.toJSONString(admin));
+    }
+
+    @Test
+    public void testUpdateAdmin() {
+        Admin admin = adminRepository.findById(1);
+        admin.setUserName("www");
+        adminRepository.update(admin);
+        admin = adminRepository.findById(1);
+        System.out.println(JSON.toJSONString(admin));
+    }
+
+    @Test
+    public void deleteById() {
+        adminRepository.deleteById(1);
+        adminRepository.deleteById(2);
     }
 }
