f35b8eb28e63e86c2f9fbc985e69c642ae8ea4d5
wenjin.wang <wenjin.wang@pintec.com>
2019-12-26 12:41:24 +0800

    coding

--- src/test/java/orc/orc/DaoTest.java
+++ src/test/java/orc/orc/DaoTest.java
@@ -1,28 +1,27 @@
 package orc.orc;
 
-import com.alibaba.fastjson.JSON;
 import orc.orc.domain.Admin;
-import orc.orc.repository.AdminRepository;
+import orc.orc.domain.Product;
+import orc.orc.repository.AdminMapper;
+import orc.orc.repository.ProductMapper;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by wenjin.wang on 2019/12/25.
  */
 public class DaoTest extends BaseTest{
     @Autowired
-    private AdminRepository adminRepository;
+    private AdminMapper adminRepository;
+    @Autowired
+    private ProductMapper productMapper;
 
     @Test
     public void testInsertAdmint() {
         Admin ad = new Admin();
-        ad.setPassword("admin2");
-        ad.setUserName("admin2");
+        ad.setPassword("admin");
+        ad.setUserName("admin");
         ad.setCreatedAt(new Date());
         ad.setCreatedBy(0);
         ad.setUpdatedAt(new Date());
@@ -30,34 +29,10 @@ public class DaoTest extends BaseTest{
         adminRepository.insert(ad);
     }
 
-    @Test
-    public void testFind() {
-        Map<String, Object> map = new HashMap();
-        map.put("userName", "admin");
-        map.put("password", "admin");
-
-        List<Admin> adminList = adminRepository.find(map);
-        System.out.println(JSON.toJSONString(adminList));
-    }
 
     @Test
-    public void testFindById() {
-        Admin admin = adminRepository.findById(1);
-        System.out.println(JSON.toJSONString(admin));
-    }
-
-    @Test
-    public void testUpdateAdmin() {
-        Admin admin = adminRepository.findById(1);
-        admin.setUserName("www");
-        adminRepository.update(admin);
-        admin = adminRepository.findById(1);
-        System.out.println(JSON.toJSONString(admin));
-    }
-
-    @Test
-    public void deleteById() {
-        adminRepository.deleteById(1);
-        adminRepository.deleteById(2);
+    public void testFindProduct() {
+        Product product = productMapper.selectByPrimaryKey(1);
+        System.out.println(product);
     }
 }
