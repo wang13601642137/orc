dc8372a9f683f795cd73f80bc7fd26df03ea37a3
wenjin.wang <wenjin.wang@pintec.com>
2019-12-26 21:36:27 +0800

    code

--- src/main/java/orc/orc/controller/TestController.java
+++ src/main/java/orc/orc/controller/TestController.java
@@ -1,6 +1,7 @@
 package orc.orc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -11,9 +12,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/")
 public class TestController {
 
-    @RequestMapping(value="/test",produces="text/html;charset=UTF-8" )
+    @RequestMapping(value = "/test", produces = "text/html;charset=UTF-8")
     @ResponseBody
-    private String test(){
+    private String test() {
         return "ok";
+    }
+
+    @RequestMapping("/testftl")
+    public String sayHello1(ModelMap map) {
+        map.put("userName", "小明");
+        map.put("userAge", 23);
+        return "test";
     }
 }
