b2f1a85557e5c6bdeecd03b26def7f7284b96f6c
wenjin.wang <wenjin.wang@pintec.com>
2019-12-27 09:18:53 +0800

    123

---
+++ src/main/java/orc/orc/controller/IndexController.java
@@ -0,0 +1,22 @@
+package orc.orc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wenjin.wang on 2019/12/27.
+ */
+
+@Controller
+@RequestMapping(value = "index")
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String sayHello1(ModelMap map) {
+        map.put("userName", "小明");
+        map.put("userAge", 23);
+        return "index";
+    }
+
+}
--- src/main/java/orc/orc/controller/LoginController.java
+++ src/main/java/orc/orc/controller/LoginController.java
@@ -3,25 +3,18 @@ package orc.orc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Created by wenjin.wang on 2019/12/3.
+ * Created by wenjin.wang on 2019/12/27.
  */
 @Controller
 @RequestMapping(value = "/")
-public class TestController {
+public class LoginController {
 
-    @RequestMapping(value = "/test", produces = "text/html;charset=UTF-8")
-    @ResponseBody
-    private String test() {
-        return "ok";
-    }
-
-    @RequestMapping("/testftl")
+    @RequestMapping("/login")
     public String sayHello1(ModelMap map) {
         map.put("userName", "小明");
         map.put("userAge", 23);
-        return "test";
+        return "login";
     }
 }
